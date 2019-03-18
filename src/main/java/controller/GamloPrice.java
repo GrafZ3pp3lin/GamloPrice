db5d5ba2aff763d7b8e2cf7557fbb69bf55d69e9
GrafZeppelin <jojo98.hannes@gmail.com>
2019-03-18 21:33:27 +0100

    .

--- src/main/java/controller/GamloPrice.java
+++ src/main/java/controller/GamloPrice.java
@@ -5,21 +5,22 @@ import data.interfaces.*;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 import service.timer.StandardTimer;
 import service.timer.TimerMode;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class GamloPrice extends Application {
 
     private Stage primaryStage;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) {
@@ -38,7 +39,47 @@ public class GamloPrice extends Application {
 
     }
 
-    private void timerTest(){
+    private void loadSampleLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/Home.fxml"));
+            loader.setController(new HomeController());
+            Parent layout = loader.load();
+            primaryStage.setScene(new Scene(layout));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void simpleTestGame() {
+
+        IGame Game = new Game("TestGame");
+        for (int j = 0; j < 5; j++) {
+            ICategory cat = new Category("TestCategory: " + j);
+            for (int i = 20; i < 100; i += 20) {
+                IQuestion question = new Question(i);
+                IQuestionData<String> questionTitle = new QuestionData<>("data");
+                questionTitle.setData("Wann ist Napoleon geboren?");
+                IQuestionComponent component = new QuestionComponent("title", "title", Arrays.asList(questionTitle));
+                IQuestionLayout layout = new QuestionLayout("Frage", Arrays.asList(component));
+                question.setQuestionLayout(layout);
+                cat.addQuestion(question);
+            }
+            Game.addCategory(cat);
+        }
+
+        //IGame Game = Global.binarySerializer.readGameFile("C:\\Users\\joels\\Desktop\\test.gp");
+        if (Game != null) {
+            GameController gc = new GameController(Game);
+            gc.showGame();
+        }
+        else {
+            System.out.println("oops, Something went wrong");
+        }
+        Global.binarySerializer.saveGame(Game, "C:\\Users\\joels\\Desktop\\test.gp");
+    }
+
+    private void timerTest() {
         StandardTimer standard = new StandardTimer(TimerMode.COUNTDOWN, 11110);
         Stage control = new Stage();
         control.setTitle("Timercontrol");
@@ -58,50 +99,5 @@ public class GamloPrice extends Application {
 
         control.show();
         show.show();
-    }
-
-    private void simpleTestGame() {
-
-        IGame Game = new Game("TestGame");
-        for(int j = 0; j < 5; j++){
-            ICategory cat = new Category("TestCategory: " + j);
-            for (int i = 20; i < 100; i += 20) {
-                IQuestion question = new Question(i);
-                IQuestionData<String> questionTitle = new QuestionData<>();
-                questionTitle.setData("Wann ist Napoleon geboren?");
-                IQuestionComponent component = new QuestionComponent("type", "title", questionTitle);
-                List<IQuestionComponent> componentList = new ArrayList<>();
-                IQuestionLayout layout = new QuestionLayout("Frage", componentList);
-                question.setQuestionLayout(layout);
-                cat.addQuestion(question);
-            }
-            Game.addCategory(cat);
-        }
-
-        //IGame Game = Global.binarySerializer.readGameFile("C:\\Users\\joels\\Desktop\\test.gp");
-        if(Game != null){
-            GameController gc = new GameController(Game);
-            gc.showGame();
-        }
-        else{
-            System.out.println("oops, Something went wrong");
-        }
-        Global.binarySerializer.saveGame(Game, "C:\\Users\\joels\\Desktop\\test.gp");
-    }
-
-    private void loadSampleLayout() {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/Home.fxml"));
-            loader.setController(new HomeController());
-            Parent layout = loader.load();
-            primaryStage.setScene(new Scene(layout));
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
